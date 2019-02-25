e908933b58a87d307e4648802cec5c80b9181e00
visualstudio <visualstudio@230-PC35>
2019-02-25 15:31:32 -0500

    javascript

---
+++ week7/js/script.js
@@ -0,0 +1,23 @@
+﻿var bird =
+{
+    genius: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOffSpring: 5,
+    noisy: true,
+    deadly: false,
+};
+var bird =
+{
+    genius: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffSpring: 3,
+    noisy: true,
+    deadly: true,
+};
+
